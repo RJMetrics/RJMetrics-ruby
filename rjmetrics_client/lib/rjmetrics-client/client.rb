e19a1ca1e3fe7fec5459fc36a8ccd752d2e85ec0
Owen Jones <owengalenjones@gmail.com>
2014-01-22 13:34:03 -0500

    initialize

--- rjmetrics_client/lib/rjmetrics-client/client.rb
+++ rjmetrics_client/lib/rjmetrics-client/client.rb
@@ -3,24 +3,27 @@ class Client
  API_BASE = "https://connect.rjmetrics.com/v2"
  SANDBOX_BASE = "https://sandbox-connect.rjmetrics.com/v2"
 
-  def initialize(client_id, api_key, timeout_in_seconds)
-    if not client_id.integer? or client_id <= 0
-      raise ArgumentError, "Invalid client ID: #{client_id} -- must be a positive integer."
-    end
-
-    if not timeout_in_seconds.integer? or timeout_in_seconds <= 0
-      raise ArgumentError, "Invalid timeout: #{timeout_in_seconds} -- must be a positive integer."
-    end
-
-    if not api_key.is_a? String
-      raise ArgumentError, "Invalid API key: #{timeout_in_seconds} -- must be a string."
-    end
+  def initialize(client_id, api_key, timeout_in_seconds = 10)
+    @client_id = client_id
+    @api_key = api_key
+    @timeout_in_seconds = timeout_in_seconds
+    validateArgs
   end
 
-  def hi
-    puts "Hello World From CLient"
-    puts API_BASE
-    puts SANDBOX_BASE
+  private
+
+  def validateArgs
+    if not @client_id.is_a? Integer or @client_id <= 0
+      raise ArgumentError, "Invalid client ID: #{@client_id} -- must be a positive integer."
+    end
+
+    if not @timeout_in_seconds.is_a? Integer or @timeout_in_seconds <= 0
+      raise ArgumentError, "Invalid timeout: #{@timeout_in_seconds} -- must be a positive integer."
+    end
+
+    if not @api_key.is_a? String
+      raise ArgumentError, "Invalid API key: #{@timeout_in_seconds} -- must be a string."
+    end
   end
 
   class UnableToConnectException < RuntimeError
--- rjmetrics_client/lib/rjmetrics_client.rb
+++ rjmetrics_client/lib/rjmetrics_client.rb
@@ -1,4 +1,8 @@
 class RJMetricsClient
+
+  def self.hi(client_id, api_key, timeout_in_seconds = 10)
+    client = Client.new(client_id, api_key, timeout_in_seconds)
+  end
 end
 
 require 'rjmetrics-client/client'
